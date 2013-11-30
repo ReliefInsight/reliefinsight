c4ffeae63bfce4ed029b05181edfc9bfb8abf817
Ken Lu <ken@seeice.com>
2013-11-30 14:29:24 -0500

    added the two amount and the calculation form model

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,12 @@ class Product < ActiveRecord::Base
   def self.request_list
     all
   end
+
+  def sum_needed_amount
+    requester_product_relations.inject(0){|sum, request| sum += request.amount}
+  end
+
+  def sum_current_amount
+    donor_product_relations.inject(0){|sum, donation| sum += donation.amount}
+  end
 end
