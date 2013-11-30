82c13d4f8d07a06217bec0087bdc07ac3bd24041
Ken Lu <ken@seeice.com>
2013-11-30 13:15:39 -0500

    configuration and the view

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Donation::Application.routes.draw do
   devise_for :admins
   devise_for :users
   resources :products
+  root to: 'home#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
